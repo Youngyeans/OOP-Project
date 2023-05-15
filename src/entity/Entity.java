412c7f6912c133658662c58b0792bddb5824911d
youngyeans <94213667+Youngyeans@users.noreply.github.com>
2023-05-16 00:02:18 +0700

    Add files via upload

---
+++ src/entity/Entity.java
@@ -0,0 +1,295 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import main.GamePanel;
+import object.TablewithCust;
+import object.topTablewithCust;
+
+//stories variables that be used in Player , Customer Class
+public class Entity {
+    GamePanel gp;
+    public int x, y;
+    public int speed;
+    
+    //image
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2, walk1, walk2, walk3, walk4;
+    public BufferedImage updish1, updish2, downdish1, downdish2, leftdish1, leftdish2, rightdish1, rightdish2;
+    public String direction;
+    
+    //make animation : change pic
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+    
+    //solid Tiles
+    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    //solid oject
+    public int solidAreaDefaultX, solidAreaDefaultY;
+    public boolean collisionOn = false;
+    
+    //for customer
+//    public int heart;
+//    public String state;
+    public int width, height;
+    public boolean hasTable;
+    public boolean hasline;
+    
+    public Entity(GamePanel gp){
+        this.gp = gp;
+    }
+    
+    
+    public void update(){
+        //setAction();
+        
+        collisionOn = false;
+        gp.cChecker.checkTile(this);
+        gp.cChecker.checkObject(this, false);
+        gp.cChecker.checkPlayer(this);
+        
+        // no line and table A
+            if(collisionOn == false && gp.obj[1].empty == true && hasTable == false){
+                if(y != 190 && y != 210){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else if(x != 120){
+                    if(direction != "to"){
+                        direction = "to";
+                        y += 20;
+                    }
+                    x += speed;
+                    spriteCounter++;
+                }
+                else{
+                    System.out.println("have customer at A");
+                    direction = "";
+                    solidArea.height = 0;
+                    solidArea.width = 0;
+                    gp.obj[1].empty = false;
+                    hasTable = true;
+                    //table A with Customer
+                    gp.obj[21] = new TablewithCust();
+                    gp.obj[21].x = 5 * gp.tileSize + 13;
+                    gp.obj[21].y = 3 * gp.tileSize + 14;
+                    gp.obj[1].empty = false;
+                    gp.obj[31] = new topTablewithCust();
+                    gp.obj[31].x = 5 * gp.tileSize + 13;
+                    gp.obj[31].y = 3 * gp.tileSize + 14;
+                }
+            }
+            
+            // no line and table B
+            else if(collisionOn == false && gp.obj[2].empty == true && hasTable == false ){
+                if(y != 114 && y!= 134){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else if(x != 400){
+                    if(direction != "to"){
+                        direction = "to";
+                        y += 20;
+                    }
+                    x += speed;
+                    spriteCounter++;
+                }
+                else{
+                    System.out.println("have customer at B");
+                    direction = "";
+                    solidArea.height = 0;
+                    solidArea.width = 0;
+                    gp.obj[2].empty = false;
+                    hasTable = true;
+                    gp.obj[22] = new TablewithCust();
+                    gp.obj[22].x = 11 * gp.tileSize - 14;
+                    gp.obj[22].y = 3 * gp.tileSize + 14;
+                    gp.obj[2].empty = false;
+                    gp.obj[32] = new topTablewithCust();
+                    gp.obj[32].x = 11 * gp.tileSize - 14;
+                    gp.obj[32].y = 3 * gp.tileSize + 14;
+
+                    
+                }
+            }
+        
+        // no line and table C
+            else if(collisionOn == false && gp.obj[3].empty == true && hasTable == false){
+                if(y != 400 && y != 420){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else if(x != 120){
+                    if(direction != "to"){
+                        direction = "to";
+                        y += 20;
+                    }
+                    x += speed;
+                    spriteCounter++;
+                }
+                else{
+                    System.out.println("have customer at C");
+                    direction = "";
+                    solidArea.height = 0;
+                    solidArea.width = 0;
+                    gp.obj[3].empty = false;
+                    hasTable = true;
+                    gp.obj[23] = new TablewithCust();
+                    gp.obj[23].x = 5 * gp.tileSize + 13;
+                    gp.obj[23].y = 7 * gp.tileSize + 7;
+                    gp.obj[3].empty = false;
+                    gp.obj[33] = new topTablewithCust();
+                    gp.obj[33].x = 5 * gp.tileSize + 13;
+                    gp.obj[33].y = 7 * gp.tileSize + 7;
+                }
+            }
+            
+         // no line and table D
+            else if(collisionOn == false && gp.obj[4].empty == true && hasTable == false){
+                if(y != 300 && y != 320){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else if(x != 400){
+                    if(direction != "to"){
+                        direction = "to";
+                        y += 20;
+                    }
+                    x += speed;
+                    spriteCounter++;
+                }
+                else{
+                    System.out.println("have customer at D");
+                    direction = "";
+                    solidArea.height = 0;
+                    solidArea.width = 0;
+                    gp.obj[4].empty = false;
+                    hasTable = true;
+                    gp.obj[24] = new TablewithCust();
+                    gp.obj[24].x = 11 * gp.tileSize - 14;
+                    gp.obj[24].y = 7 * gp.tileSize + 7;
+                    gp.obj[4].empty = false;
+                    gp.obj[34] = new topTablewithCust();
+                    gp.obj[34].x = 11 * gp.tileSize - 14;
+                    gp.obj[34].y = 7 * gp.tileSize + 7;
+                }
+            }
+            
+            //no table get line
+            else if(collisionOn == false && hasTable == false && gp.line == 0){
+                if(y != 400){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else{
+                    gp.line = 1;
+                    speed = 0;
+                    hasline = true;
+                    spriteNum = 1;
+                }
+            }
+            else if(collisionOn == false && hasTable == false && gp.line == 1){
+                if(y != 250){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else{
+                    gp.line = 2;
+                    speed = 0;
+                    hasline = true;
+                    spriteNum = 1;
+                }
+            }
+            else if(collisionOn == false && hasTable == false && gp.line == 2){
+                if(y != 100){
+                    y += speed;
+                    spriteCounter++;
+                }
+                else{
+                    gp.line = 3;
+                    speed = 0;
+                    hasline = true;
+                    spriteNum = 1;
+                }
+            }
+              
+//                switch(direction){
+//                    case "walk":
+//                        
+//                        break;
+//                    case "down":
+//                        y += speed;
+//                        break;
+//                    case "left":
+//                        x -= speed;
+//                        break;
+//                    case "right":
+//                        x += speed;
+//                        break;
+//                }
+            //set time to change
+            //spriteCounter++;
+            if(spriteCounter > 12){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+    }
+    
+    public void draw(Graphics2D g2){
+        BufferedImage image = null;
+        
+        switch(direction){
+            case "walk":
+                if(spriteNum == 1){
+                    image = walk1;
+                }
+                if(spriteNum == 2){
+                    image = walk2;
+                }
+                break;
+            case "to":
+                width = 135;
+                height = 50;
+                solidArea.width = width;
+                solidArea.height = height;
+                if(spriteNum == 1){
+                    image = walk3;
+                }
+                if(spriteNum == 2){
+                    image = walk4;
+                }
+                break;
+//            case "down":
+//                if(spriteNum == 1){
+//                    image = down1;
+//                }
+//                if(spriteNum == 2){
+//                    image = down2;
+//                }
+//                break;
+//            case "left":
+//                if(spriteNum == 1){
+//                    image = left1;
+//                }
+//                if(spriteNum == 2){
+//                    image = left2;
+//                }
+//                break;
+//            case "right":
+//                if(spriteNum == 1){
+//                    image = right1;
+//                }
+//                if(spriteNum == 2){
+//                    image = right2;
+//                }
+//                break;
+        }
+        //g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, x, y, width, height, null);
+    }
+}
---
+++ src/entity/Player.java
@@ -0,0 +1,326 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import main.GamePanel;
+import main.KeyHandler;
+import object.ShopnoNoodle;
+import object.TablewithDish;
+import object.topTablewithDish;
+
+public class Player extends Entity{
+    GamePanel gp;
+    KeyHandler keyH;
+    
+    int hasKey = 0;
+    
+    public Player(GamePanel gp, KeyHandler keyH){
+        
+        super(gp);
+        this.gp = gp;
+        this.keyH = keyH;
+        
+        //solid (x, y, width, height)
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 20;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 50;
+        solidArea.height = 35;
+        
+        setDefaultValues();
+        getPlayerImage();
+    }
+    
+    public void setDefaultValues(){
+        x = 13 * gp.tileSize + 10;
+        y = 2 * gp.tileSize + 20;
+        speed = 4;
+        direction = "down";
+    }
+    
+    //getimage
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(new File("res/player/boy_up_1.png"));
+            up2 = ImageIO.read(new File("res/player/boy_up_2.png"));
+            down1 = ImageIO.read(new File("res/player/boy_down_1.png"));
+            down2 = ImageIO.read(new File("res/player/boy_down_2.png"));
+            left1 = ImageIO.read(new File("res/player/boy_left_1.png"));
+            left2 = ImageIO.read(new File("res/player/boy_left_2.png"));
+            right1 = ImageIO.read(new File("res/player/boy_right_1.png"));
+            right2 = ImageIO.read(new File("res/player/boy_right_2.png"));
+            updish1 = ImageIO.read(new File("res/player/updish1.png"));
+            updish2 = ImageIO.read(new File("res/player/updish2.png"));
+            downdish1 = ImageIO.read(new File("res/player/downdish1.png"));
+            downdish2 = ImageIO.read(new File("res/player/downdish2.png"));
+            leftdish1 = ImageIO.read(new File("res/player/leftdish1.png"));
+            leftdish2 = ImageIO.read(new File("res/player/leftdish2.png"));
+            rightdish1 = ImageIO.read(new File("res/player/rightdish1.png"));
+            rightdish2 = ImageIO.read(new File("res/player/rightdish2.png"));
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    //change posiition
+    public void update(){
+        if(keyH.upPressed == true || keyH.downPressed == true || 
+                keyH.leftPressed == true || keyH.rightPressed == true){
+            if(keyH.upPressed == true){
+                direction = "up";
+            }
+            else if(keyH.downPressed == true){
+                direction = "down";
+            }
+            else if(keyH.leftPressed == true){
+                direction = "left";
+            }
+            else if(keyH.rightPressed == true){
+                direction = "right";
+            }
+            
+            //check tiles collision
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+            
+            //check obj collision
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+           
+            //check customer collision
+            int custIndex = gp.cChecker.checkEntity(this, gp.cust);
+            
+            
+            //no collision = can move
+            if(collisionOn == false){
+                switch(direction){
+                    case "up":
+                        y -= speed;
+                        break;
+                    case "down":
+                        y += speed;
+                        break;
+                    case "left":
+                        x -= speed;
+                        break;
+                    case "right":
+                        x += speed;
+                        break;
+                }
+            }
+            //set time to change
+            spriteCounter++;
+            if(spriteCounter > 12){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+    }
+    
+    public void pickUpObject(int i) {
+        
+   
+
+        if(keyH.pickUpPressed == true){
+            if(i == 0){
+                String objectName = gp.obj[i].name; 
+                
+                switch(objectName){
+                    case("Shop"):
+                      
+                        gp.obj[19] = new ShopnoNoodle();
+                        gp.obj[19].x = 7 * gp.tileSize + 23;
+                        gp.obj[19].y = 0;
+
+                        hasKey+=3;
+                        gp.obj[i] = null;
+                        System.out.println("key : "+ hasKey);
+                        
+                        
+                }       
+            }
+            
+            else if(i == 1){
+                String objectName = gp.obj[i].name;
+            
+                switch(objectName){
+                    case("Table"):
+                        if(hasKey > 0){
+                            gp.obj[6] = new TablewithDish();
+                            gp.obj[6].x = 5 * gp.tileSize + 13;
+                            gp.obj[6].y = 3 * gp.tileSize + 14;
+                            
+                            gp.obj[11] = new topTablewithDish();
+                            gp.obj[11].x = 5 * gp.tileSize + 13;
+                            gp.obj[11].y = 3 * gp.tileSize + 14;
+                            
+                            gp.obj[1] = null;
+                            hasKey-- ;
+                            
+                            System.out.println("key : "+ hasKey);
+                    
+                        }       
+                        
+                } 
+            }
+            
+            else if(i == 2){
+                String objectName = gp.obj[i].name;
+            
+                switch(objectName){
+                    case("Table"):
+                        if(hasKey > 0){
+                            gp.obj[7] = new TablewithDish();
+                            gp.obj[7].x = 11 * gp.tileSize - 14;
+                            gp.obj[7].y = 3 * gp.tileSize + 14;
+                            
+                            gp.obj[12] = new topTablewithDish();
+                            gp.obj[12].x = 11 * gp.tileSize - 14;
+                            gp.obj[12].y = 3 * gp.tileSize + 14;
+                            
+                            gp.obj[2] = null;
+                            hasKey-- ;
+                            System.out.println("key : "+ hasKey);
+                           
+                        }
+                } 
+            }  
+            else if(i == 3){
+                String objectName = gp.obj[i].name;
+            
+                switch(objectName){
+                    case("Table"):
+                        if(hasKey > 0){
+                            gp.obj[8] = new TablewithDish();
+                            gp.obj[8].x = 5 * gp.tileSize + 13;
+                            gp.obj[8].y = 7 * gp.tileSize + 7;
+                            
+                            gp.obj[13] = new topTablewithDish();
+                            gp.obj[13].x = 5 * gp.tileSize + 13;
+                            gp.obj[13].y = 7 * gp.tileSize + 7;
+                            
+                            gp.obj[3] = null;
+                            hasKey-- ;
+                            System.out.println("key : "+ hasKey);
+                             
+                        }
+                } 
+            }
+            
+            else if(i == 4){
+                String objectName = gp.obj[i].name;
+            
+                switch(objectName){
+                    case("Table"):
+                        if(hasKey > 0){
+                            gp.obj[9] = new TablewithDish();
+                            gp.obj[9].x = 11 * gp.tileSize - 14;
+                            gp.obj[9].y = 7 * gp.tileSize + 7;
+                            
+                            gp.obj[14] = new topTablewithDish();
+                            gp.obj[14].x = 11 * gp.tileSize - 14;
+                            gp.obj[14].y = 7 * gp.tileSize + 7;
+                            
+                            gp.obj[4] = null;
+                            hasKey-- ;
+                            System.out.println("key : "+ hasKey);
+           
+                        }
+                } 
+            }
+        }
+    }
+    
+    //make walking animetion
+  public void draw(Graphics2D g2){        
+        BufferedImage image = null;
+        if(hasKey == 0){
+            switch(direction){
+                case "up":
+                    if(spriteNum == 1){
+                        image = up1;
+                    }
+                    if(spriteNum == 2){
+                        image = up2;
+                    }
+                    break;
+                case "down":
+                    if(spriteNum == 1){
+                        image = down1;
+                    }
+                    if(spriteNum == 2){
+                        image = down2;
+                    }
+                    break;
+                case "left":
+                    if(spriteNum == 1){
+                        image = left1;
+                    }
+                    if(spriteNum == 2){
+                        image = left2;
+                    }
+                    break;
+                case "right":
+                    if(spriteNum == 1){
+                        image = right1;
+                    }
+                    if(spriteNum == 2){
+                        image = right2;
+                    }
+                    break;
+            }
+        }
+        else if(hasKey >= 1){
+            switch(direction){
+                case "up":
+                    if(spriteNum == 1){
+                        image = updish1;
+                    }
+                    if(spriteNum == 2){
+                        image = updish2;
+                    }
+                    break;
+                case "down":
+                    if(spriteNum == 1){
+                        image = downdish1;
+                    }
+                    if(spriteNum == 2){
+                        image = downdish2;
+                    }
+                    break;
+                case "left":
+                    if(spriteNum == 1){
+                        image = leftdish1;
+                    }
+                    if(spriteNum == 2){
+                        image = leftdish2;
+                    }
+                    break;
+                case "right":
+                    if(spriteNum == 1){
+                        image = rightdish1;
+                    }
+                    if(spriteNum == 2){
+                        image = rightdish2;
+                    }
+                    break;
+            }
+            
+        }
+        //g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, x, y, 69, 69, null);
+        
+    }
+}
